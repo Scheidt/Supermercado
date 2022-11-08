a659af541b0ce0b04688db4a953c3e41e452e56f
Scheidt <112341474+Scheidt@users.noreply.github.com>
2022-11-08 14:05:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 from controlador.controlador_sistema import ControladorSistema
 
+
+# Este trabalho foi feito em dupla. Eu fiz tanto os produtos quanto os carrinhos. Também auxiliei minha dupla na parte do cliente,
+# mas não posso assumir toda a responsabilidade.
 ControladorSistema().abre_tela()
