39e2c6fa5d103817651a8da0499d034759692056
Scheidt <pedrohenriqueprazeres@hotmail.com>
2026-08-14 14:02:11 -0300

    Style: Removed unused/legacy files

--- excecao/naohacarrinho.py
+++
@@ -1,5 +0,0 @@
-
-
-class NaoHaCarrinhoException(Exception):
-    def __init__(self):
-        super().__init__("Não há carrinho para este cpf.")
--- excecao/produtonaoencontrado.py
+++
@@ -1,5 +0,0 @@
-
-
-class ProdutoNaoEncontradoException(Exception):
-    def __init__(self) -> None:
-        super().__init__("Não foi encontrado produto para esta ID.")
